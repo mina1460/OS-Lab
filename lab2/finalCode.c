af714417b1f9d5adfeb718fcd6228fd0f5935c89
User <user@localhost.localdomain>
2021-02-22 18:31:58 +0200

    commented

--- lab2/finalCode.c
+++ lab2/finalCode.c
@@ -11,10 +11,10 @@
 
 #define BUFFER_LEN 1024
 
-char infile [1024];
-char outfile [1024];
-char * between_ticks;
-char* tick_var;
+char infile [1024]; 	/*input file name*/
+char outfile [1024]; 	/*output file name*/
+char * between_ticks; 	/*the command between the ticks*/
+char* tick_var; 		/*the variable before the tick*/
 
 extern char **environ;
 
@@ -25,7 +25,7 @@ void set_cmd(int argc, char *argv[])
 	printf("Extra args\n");
 	else
 	for (i = 0; environ[i] != NULL; i++)
-	printf("%s\n", environ[i]);
+	printf("%s\n", environ[i]);			/* print each entry in the environ as long as it is not NULL */
 }
 
 void asg(int argc, char *argv[])
@@ -34,25 +34,29 @@ void asg(int argc, char *argv[])
 	if (argc != 1)
 		printf("Extra args\n");
 	else {
-		name = (char *) malloc(1024);
-		val = (char *) malloc(2048);
-		name = strtok(argv[0], "=");
-		val = strtok(NULL, "");
+		name = (char *) malloc(1024); 	/*allocate space for the variable name*/
+		val = (char *) malloc(2048);	/*allocate space for the value assigned to it*/
+		name = strtok(argv[0], "=");	/*parse the name until the equal sign */
+		val = strtok(NULL, "");			/*parse whatever comes after the equal sign */
 		
 		if (name == NULL || val == NULL)
-			printf("Bad command\n");
+			printf("Failed: Name or Value is NULL\n"); /*if either of them is NULL, then print this message*/
 		else
 			{
 				char n[1024];
 				char v[2048];
 				strcpy(n, name);
 				strcpy(v, val);
-				setenv(n, v, true);
+				setenv(n, v, true);	/*set the environment with overwrite enabled*/
 			}
 
 	}
 return;
 }
+/*this function is from geeks for geeks I used it and I am referencing it here: 
+https://www.geeksforgeeks.org/c-program-replace-word-text-another-given-word/
+I could have changed the varibale name and reordered it, but this would be dishonet from me; However, I have read the code and understood it and cited it.
+*/
 char* replaceWord(const char* s, const char* oldW, 
                   const char* newW) 
 { 
@@ -73,15 +77,15 @@ char* replaceWord(const char* s, const char* oldW,
     i = 0; 
     while (*s) { 
         if (strstr(s, oldW) == s) { 
-            strcpy(&result[i], newW); 
-            i += newWlen; 
-            s += oldWlen; 
+            strcpy(&result[i], newW); /*copy the new word in the place of the old word if found*/
+            i += newWlen; /*go over the new word*/
+            s += oldWlen;  /*skip the old char*/
         } 
         else
-            result[i++] = *s++; 
+            result[i++] = *s++;  /*copy one more character*/
     } 
   
-    result[i] = '\0'; 
+    result[i] = '\0'; /*adds a null terminator at the end*/
     return result; 
 } 
 
@@ -100,16 +104,16 @@ size_t read_command(char *cmd)
 
 int build_args(char * cmd_o, char ** argv, int* in, int *out) {
  	char *token; 
- 	char *cmd = (char *) malloc(BUFFER_LEN);
+ 	char *cmd = (char *) malloc(BUFFER_LEN); /*a local copy of the command to work on it safely */
  
- 	strcpy(cmd, cmd_o);
+ 	strcpy(cmd, cmd_o); /*copy it into the buffer*/
 
-	token = strtok(cmd," ");
+	token = strtok(cmd," "); /*parse the first token so that the while loop works*/
 	int i=0;
 	
 	 while(token!=NULL){
 	 
-	 	if (strcmp(token, ">")==0)
+	 	if (strcmp(token, ">")==0) /*if found output redirection, parse file name and set the out var to true*/
 	 	{
 	 		token = strtok(NULL, " ");	
 	 		char* t_out_file;
@@ -118,7 +122,7 @@ int build_args(char * cmd_o, char ** argv, int* in, int *out) {
 	 		strcpy(outfile, t_out_file);
 	 		*out = 1;
 	 	}
-	 	else if (strcmp(token, "<")==0)
+	 	else if (strcmp(token, "<")==0) /*if found input redirection, parse file name and set the in var to true*/
 	 	{
 	 		token = strtok(NULL, " ");	
 	 		char* t_in_file;
@@ -127,7 +131,7 @@ int build_args(char * cmd_o, char ** argv, int* in, int *out) {
 	 		strcpy(infile, t_in_file);
 	 		*in = 1;
 	 	}
-	 	else {
+	 	else { /*otherwise, just copy the token into argv*/
 	 		argv[i] = (char * ) malloc(1024);
 	 		strcpy(argv[i], token);
 	 		
@@ -137,7 +141,7 @@ int build_args(char * cmd_o, char ** argv, int* in, int *out) {
 	 	}
 	 	
 	 }
- argv[i]=NULL; 
+ argv[i]=NULL;  /*set the last one to null so that execvp works*/
 	return i; 
 }
 
@@ -147,7 +151,7 @@ void set_program_path (char * path, char * bin, char * prog) {
  	strcat(path, prog); 
  	int i=0;
 	for(; i<strlen(path); i++) 
-		if(path[i]=='\n') path[i]='\0';
+		if(path[i]=='\n') path[i]='\0'; 
 }
 
 void handle_spaces(char* cmd){
@@ -166,6 +170,7 @@ void handle_spaces(char* cmd){
 		bool c = false;
 		for ( ;j < special_c; j++)
 		{
+			/*loop over the special characters and if found add a space before and after it*/
 			if (cmd[i] == special[j])
 			{
 				c = true;
@@ -176,25 +181,25 @@ void handle_spaces(char* cmd){
 		}
 		if (!c)
 		{
-			  strncat(mod_cmd, &cmd[i], 1);
+			  strncat(mod_cmd, &cmd[i], 1); /*else, copy a char by char */
 		}
 	}
-	strcpy(cmd, mod_cmd);
+	strcpy(cmd, mod_cmd); /*return the modified string*/
 	return;
 }
 
 bool check_ticks(char* cmd){
 	if(strchr(cmd, '`') != NULL){
-		char * x = strchr(cmd, '`');
-		char * y = strrchr(cmd, '`');
-		if (x==y) return false;
-		else return true;
+		char * x = strchr(cmd, '`'); /*the position of the first tick*/
+		char * y = strrchr(cmd, '`'); /*the position of the last tick*/
+		if (x==y) return false; /*they must point to two different locations*/
+		else return true; /*if they are not equal then we have valid ticks*/
 	}
-	else return false;
+	else return false; /*if no ticks are found then return false*/
 }
 void parse_ticks(char* cmd){
 	char * parsed_cmd = (char *) calloc(1024, sizeof(char));
-	strncat(parsed_cmd, strchr(cmd, '`')+1, (strrchr(cmd, '`')-strchr(cmd, '`')-sizeof(char))/sizeof(char));
+	strncat(parsed_cmd, strchr(cmd, '`')+1, (strrchr(cmd, '`')-strchr(cmd, '`')-sizeof(char))/sizeof(char)); /*parse starting from the location of the first tick till the character before the final tick*/
 	parsed_cmd[(strrchr(cmd, '`')-strchr(cmd, '`'))] = '\0'; 
 	strcpy(between_ticks, parsed_cmd);
 }
@@ -217,73 +222,73 @@ int main(){
 		  "cd", 
 		  "clear", 
 		  "set"
-	};
+	};  /*special commands to handle*/
 
  const char *homedir;
 
  if ((homedir = getenv("HOME")) == NULL) 
-       homedir = getpwuid(getuid())->pw_dir;
+       homedir = getpwuid(getuid())->pw_dir; /*gets the homedir if it is not in the environ */
 		
 
- if (chdir(homedir)==-1) printf("Cannot start from %s \n", homedir);	
+ if (chdir(homedir)==-1) printf("Cannot start from %s \n", homedir);	/*cd into it and if not possible print the error*/
 
  while(true)
  {
- 		char* piped_cmds[100];
+ 		char* piped_cmds[100]; /*array of commands separated by pipes */
 		long size;
 		char *buf;
 		char *ptr;
 		
 		
- 		int p[2];
+ 		int p[2]; /*array of file descriptors used in pipe()*/
 
-		int in = 0; 
-		int out = 0;
+		int in = 0; /*bool to indicate if we have input redirection*/
+		int out = 0; /*for output redirection*/
 
 
  		size = pathconf(".", _PC_PATH_MAX);
 		if ((buf = (char *)malloc((size_t)size)) != NULL)
     		ptr = getcwd(buf, (size_t)size);
-		char * user_name = getenv("USER");
+		char * user_name = getenv("USER"); /*get the user name to print it in the prompt */
 
-		printf("[%s@My shell]——[%s]\n$ ", user_name, ptr); 
-		if (read_command(line) == 0 )
+		printf("[%s@My shell]——[%s]\n$ ", user_name, ptr); /*print the prompt format*/
+		if (read_command(line) == 0 ) /*read the command */
 		{
-			printf("\n"); 
+			printf("\n"); /*print a new line after it*/
 			exit(0);
 		} 
 		
-		if (strcmp(line, "exit") == 0) exit(0); 
+		if (strcmp(line, "exit") == 0) exit(0); /*if he entered exit,then exit*/
 
 
 		strcpy(line2, line); 
- 		if(strchr(line2, '$') != NULL){
+ 		if(strchr(line2, '$') != NULL){ /*handle environment variables if you found the dollar sign*/
 			char *bef, *var;
 			bef = (char*) malloc(1024);
 			char* content = (char*) malloc(1024);
 			var = (char*) malloc(1024);
-			bef = strtok(line2, "$");
-			var = strtok(NULL, " ");
-			content = getenv(var);
+			bef = strtok(line2, "$"); /*whatever comes before the '$' */
+			var = strtok(NULL, " "); /*the variable after the dollar sign */
+			content = getenv(var); /*the content of the variable from the environ */
 			char v[1024] = "$";
 			strcat(v, var);
-			strcpy( line2,replaceWord(line, v, content));
+			strcpy( line2,replaceWord(line, v, content)); /*replace the $var with the value */
 		
 		}
 		strcpy(line, line2);
-		handle_spaces(line);
+		handle_spaces(line); /*add spaces before and after special characters for correct parsing and execution */
 		strcpy(line2, line);
-		bool ticks = check_ticks(line2);
+		bool ticks = check_ticks(line2); /*check if there are ticks `cat whatever`*/
 
 		tick_var = (char * ) malloc(1024);
 		if (ticks)
 		{
 			
-			tick_var = strtok(line2, " =");
+			tick_var = strtok(line2, " ="); /*parse the var name before the equal sign if we have tickss*/
 			char ticks_cmd [BUFFER_LEN];	
-			between_ticks = (char *) calloc(1024, sizeof(char));
-			parse_ticks(line);
-			strcpy(line, between_ticks);
+			between_ticks = (char *) calloc(1024, sizeof(char)); /*allocate memory for the command between the ticks*/
+			parse_ticks(line);									/*parse the command between the ticks*/
+			strcpy(line, between_ticks);						/*copy it into the line command which is going to be executed*/
 		}
 		char tick_var_cp [100];
 		if (ticks)
@@ -300,56 +305,58 @@ int main(){
 
 		while(parsed_cmd != NULL){
 		 	strcpy(cp_parsed_cmd, parsed_cmd);
-			piped_cmds[k] = (char *) malloc(BUFFER_LEN);
-			strcpy(piped_cmds[k], cp_parsed_cmd);
+			piped_cmds[k] = (char *) malloc(BUFFER_LEN); /*allocate size for each command in the pipe*/
+			strcpy(piped_cmds[k], cp_parsed_cmd); /*copy each of them*/
 			num_commands++;
-		    parsed_cmd = strtok(NULL, delim);
+		    parsed_cmd = strtok(NULL, delim); /*read until a pipe is found*/
 		    ++k;
 		}
 		int c = 0;
 		for (c = 0; c < k; c++)
 			{
+				/*build the args for each command */
 				argc = build_args(piped_cmds[c], argv[c], &in, &out);
 			}
 		
-	int temp_in_f = dup(0);
-	int temp_out_f = dup(1);
-	int pid;
+	int temp_in_f = dup(0); /*duplicate STDIN*/
+	int temp_out_f = dup(1); /*duplicate STDOUT*/
+	int pid; /*save process id for checking later on in the fork*/
 	int loop_cmd = 0;
 			
 for (loop_cmd = 0; loop_cmd < k; loop_cmd++)
 {
 	if (loop_cmd == 0)
 	{
-		p[0] = dup(temp_in_f);
+		p[0] = dup(temp_in_f); /*save the stdin into p[0]*/
 		if (in){
 					
-					p[0] = open(infile, O_RDONLY);
+					p[0] = open(infile, O_RDONLY);	/*if we have input redirection open the input file and save it as the input source in the pipe*/
 					if (p[0] < 0)
 					{
 						perror("failed to open input file: ");
 					}
-					strcpy(infile, "");
+					strcpy(infile, ""); /*reset it*/
 
 				}
 
 	}
 	
-	dup2(p[0], 0);
-	close(p[0]);
+	dup2(p[0], 0); 	/*duplicate it into the 0 file descriptor*/
+	close(p[0]);	/*close the open descriptor*/
 	
-	if (loop_cmd == k - 1)
+	if (loop_cmd == k - 1) /*if it was the last command*/
 	{
-		p[1] = dup(temp_out_f);
+		p[1] = dup(temp_out_f); /*set the pipe descriptor to the stdout*/
 		if (out)
 				{
+					/*but if we have output redirection open the file and write to it*/
 						p[1] = open(outfile,  O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-						strcpy(outfile, "");
+						strcpy(outfile, ""); /*reset output file name*/
 				}
 				else if(ticks){
-					int fdpipe[2];
+					int fdpipe[2]; 
 					if(pipe(fdpipe)<0) perror("failure in piping: ");
-					p[1] = fdpipe[1];
+					p[1] = fdpipe[1]; /*if there are ticks, write to it*/
 					p[0] = fdpipe[0];
 
 				}		
@@ -373,29 +380,31 @@ for (loop_cmd = 0; loop_cmd < k; loop_cmd++)
  		bool  set = false;
 		for (i = 0; i < special_count; ++i)
 		{
-			if(strcmp(argv[loop_cmd][0],commands[i]) == 0){
+			if(strcmp(argv[loop_cmd][0],commands[i]) == 0){ /*compare the command with the special commands to check if a matach is found*/
 				set = true;
 				break;
 			}
+			
 		}
+
 		int x;
 		if (set)
 		{
 			switch(i){
 			case 0: 
 				if (argv[loop_cmd][1] == NULL)
-					x = chdir(homedir);
+					x = chdir(homedir); /*if typed cd only, then cd into the home dir*/
 				else
-					x = chdir(argv[loop_cmd][1]);
+					x = chdir(argv[loop_cmd][1]); /*else, cd into the desired directory*/
 				
 					if (x != 0)
 						perror("could not change directory\n");
 				break;
 				case 1:
-					system("cls||clear");
+					system("cls||clear"); /*clear screen*/
 					break;
 				case 2:
-					set_cmd(argc, argv[loop_cmd]);
+					set_cmd(argc, argv[loop_cmd]); /*print environ*/
 					break;
 			default:
 				break;
@@ -423,12 +432,13 @@ for (loop_cmd = 0; loop_cmd < k; loop_cmd++)
 	
 }	
 			char buffer[2*BUFFER_LEN];
-			char buf2[2*BUFFER_LEN];
+			/*char buf2[2*BUFFER_LEN];*/
 			if (ticks){
-				read(p[0], buffer, sizeof(buffer));
-				strncat(buf2, tick_var_cp, strlen(tick_var_cp)*sizeof(char));
+				read(p[0], buffer, sizeof(buffer)); /*read from the pipe*/
+				
+			/*	strncat(buf2, tick_var_cp, strlen(tick_var_cp)*sizeof(char));
 				strncat(buf2, "=", sizeof(char));
-				strncat(buf2, buffer, strlen(buffer)*sizeof(char));
+				strncat(buf2, buffer, strlen(buffer)*sizeof(char));*/
 			
 				if (setenv (tick_var_cp, buffer, true) < 0){
 					perror("Error setting environment variable");
